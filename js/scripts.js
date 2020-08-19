94cfce7a43cd5b88b8058da44c729b59a998661f
Grant Eadie <grantleadie@gmail.com>
2020-08-19 13:49:52 -0700

    clean code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 function isLeapYear(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+  if (isNaN(year)) {
+    return "Please enter a number";
+  } else if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return "This is a Leap Year";
   } else {
     return "This is not a Leap Year";
   }
 }
 
-$(document).ready(function(){
-  $("#formOne").submit(function(event){
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
       event.preventDefault();
     let year = parseInt($("input#userInput").val());
-
-    alert(isLeapYear(year));
-
+    $("#output").text(isLeapYear(year));
   });
 });
